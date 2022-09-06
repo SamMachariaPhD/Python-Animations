e3703aafa0e69f2ebfa03da9c51c5063c38d40b1
sirmaxford <sirmaxford@gmail.com>
2022-09-07 02:51:06 +0300

    python 1d airplane anim

--- src/1.1D-forward-motion-of-an-airplane/1D_forward_airplane.py
+++ src/1.1D-forward-motion-of-an-airplane/1D_forward_airplane.py
@@ -1,4 +1,5 @@
 # Import the needed libraries. 
+
 import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
@@ -19,11 +20,22 @@ x = 800*t # [kms]
 
 # Create an array for y (altitude)
 alt_0 = 2; alt_end = 2; 
-y = np.linspace(alt_0, alt_end, len(x))
+y = np.linspace(alt_0, alt_end, len(x)) # "[0]" index at the end or "y, =" should give the 1st element
 #print(y)
+#exit()
 
 # Create the animation 
 
-frames = len(t)
-fig = plt.figure(figsize=(40*cm,20*cm), dpi=120) # facecolor(.8, .8, .8)
-anim = animation.FuncAnimation()
+fig = plt.figure(figsize=(40*cm,20*cm), dpi=120) # facecolor=(.8, .8, .8)
+gs = gridspec.GridSpec(2,2)
+
+ax0 = fig.add_subplot(gs[0,:]) # facecolor
+trajectory = ax0.plot([],[])[0]
+plt.xlim(x[0], x[-1]); plt.ylim(0,y[-1]+1)
+
+def animate(frame): # update plots
+    trajectory.set_data(x[0:frame], y[0:frame])
+    return trajectory,
+
+animation.FuncAnimation(fig, animate, frames=len(t), interval=20, repeat=True, blit=True) # blit should be true for faster fig update
+plt.show()
